7191f37615ea85b271f64394a499c2c97e213389
antonlazukin <kevin6121991@gmail.com>
2023-07-17 15:13:56 +0300

    92

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,6 +1,6 @@
 export const jwtConstants = {
   secret: '12345678',
   secretFromEnv: process.env.JWT_SECRET, // not working
-  ACCESS_TOKEN_LIFE_TIME : '30000s',
-  REFRESH_TOKEN_LIFE_TIME : '60000s',
+  ACCESS_TOKEN_LIFE_TIME : '10s',
+  REFRESH_TOKEN_LIFE_TIME : '20s',
 };
