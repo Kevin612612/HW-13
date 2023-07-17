50f6cee3cd3094b6c12af130664f76fac7a78d00
Kevin612612 <73292632+Kevin612612@users.noreply.github.com>
2023-07-17 19:29:35 +0400

    Update constants.ts

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,6 +1,6 @@
 export const jwtConstants = {
   secret: '12345678',
   secretFromEnv: process.env.JWT_SECRET, // not working
-  ACCESS_TOKEN_LIFE_TIME : '100s',
+  ACCESS_TOKEN_LIFE_TIME : '10s',
   REFRESH_TOKEN_LIFE_TIME : '200s',
 };
